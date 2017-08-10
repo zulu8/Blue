461bc2451f761aad1beb9324ca278855042a70d7
zulu8 <cybercybertechnicalcyber@gmail.com>
2017-08-09 23:08:54 -0400

    sysmon install and restore working

--- Deploy-Blue.ps1
+++ Deploy-Blue.ps1
@@ -78,6 +78,71 @@ regKey,name,value,type
 "@
 
 
+
+$sysmonConfig = @"
+<?xml version="1.0"?>
+<Sysmon schemaversion="3.10">
+  <!-- Capture SHA256 and IMPHASH Hashes -->
+  <HashAlgorithms>SHA256,IMPHASH</HashAlgorithms>
+  <EventFiltering>
+    <!-- Log all drivers loads except if the signature is Microsoft Windows -->
+  	<DriverLoad onmatch="exclude">
+          <Signature condition="is">Microsoft Windows</Signature>
+    </DriverLoad>
+	<!-- Log all images except if it's Microsoft or Windows signed -->
+	<ImageLoad onmatch="exclude">
+		<Signature condition="is">Microsoft Windows</Signature>
+		<Signature condition="is">Microsoft Corporation</Signature>
+	</ImageLoad>
+	<!-- log only images loaded  from user profile directory, clear some noise and also monitor what is loaded  on lsass.exe> -->
+	<ImageLoad onmatch="include">
+		<Image condition="end with">lsass.exe</Image>
+		<Image condition="contains">C:\Users</Image>
+	</ImageLoad>
+    <!-- Log all CreateRemoteThread, exclude CSRSS and Winlogon -->
+	<CreateRemoteThread onmatch="exclude">
+		<SourceImage condition="is">C:\Windows\System32\csrss.exe</SourceImage>
+		<SourceImage condition="is">C:\Windows\System32\winlogon.exe</SourceImage>
+	</CreateRemoteThread>
+	<!-- Log all raw disk access if the Image is System or svchost  -->
+	<RawAccessRead onmatch="include">
+     <Image condition="contains">C:\Windows\System32\</Image>
+    <Image condition="contains">C:\Users</Image>
+    <Image condition="contains">C:\$recycle.bin</Image>
+	</RawAccessRead>
+	<!-- Log all file modified creation time -->
+	<FileCreateTime onmatch="exclude"/>
+	<!-- Log process access -->
+	<!-- Only capture access to lsass and winlogon -->
+	<ProcessAccess onmatch="include">
+		<TargetImage condition="is">C:\Windows\system32\lsass.exe</TargetImage>
+		<TargetImage condition="is">C:\Windows\system32\winlogon.exe</TargetImage>
+	</ProcessAccess>
+	<ProcessAccess onmatch="exclude">
+		<SourceImage condition="is">C:\WINDOWS\System32\Taskmgr.exe</SourceImage>
+	</ProcessAccess>
+	<!-- Log all initiated network connection -->
+	<NetworkConnect onmatch="exclude">
+       <Image condition="contains">chrome.exe</Image>
+       <Image condition="contains">iexplore.exe</Image>
+       <Image condition="contains">firefox.exe</Image>
+       <Image condition="contains">outlook.exe</Image>
+       <Image condition="contains">Skype.exe</Image>
+       <Image condition="contains">lync.exe</Image>
+       <Image condition="contains">GoogleUpdate.exe</Image>
+	   <Image condition="contains">qbittorrent.exe</Image>
+  </NetworkConnect>
+	<!-- Log all process creation -->
+	<ProcessCreate onmatch="exclude"/>
+    <!-- Do not log process termination -->
+	<ProcessTerminate onmatch="include"/>
+  </EventFiltering>
+</Sysmon>
+"@
+
+
+
+
 function Configure-Sensors
 {
 
@@ -156,21 +221,52 @@ function Configure-Sensors
 		} -Args (,$regConfig)
 
 		# Pull wecNum value from remote system
-		$global:wecNum = Invoke-Command -Session $s -Script {$wecNum}
+		$wecNum = Invoke-Command -Session $s -Script {$wecNum}
 		Write-Warning "wecNum = $wecNum"
 
-		# Backup all configs to single .ps1 file named <hostname>.config.ps1
+	# Install Sysmon
+		Copy-Item "C:\Sysmon64.exe" -Destination "C:\Sysmon64.exe" -ToSession $s
+
+		$sysmonBackup = Invoke-Command -Session $s -ScriptBlock {
+			param([string[]]$sysmonConfig)
+			if ((Get-Service Sysmon) -ne $null) {
+				Write-Warning "Sysmon service present. Documenting Config."
+				$sysmonBackup = $(&sysmon -c)
+				$sysmonBackup #| select -Skip 5
+				Remove-Item "C:\Sysmon64.exe"
+			}
+			else {
+				Write-Warning "Sysmon DNE. Installing"
+				#Copy-Item "C:\Sysmon64.exe" -Destination "C:\Sysmon64.exe" -ToSession $s
+				&C:\Sysmon64.exe -i -accepteula > null 2>&1
+				Write-Warning "Changing config to desired state."
+				$sysmonConfig | Out-File "C:\sysmonConfig.xml"
+				&C:\Sysmon64.exe -c "C:\sysmonConfig.xml" > null 2>&1
+				Remove-Item "C:\Sysmon64.exe"
+				$sysmonBackup = "DNE"
+				$sysmonBackup
+			}
+		} -Args (,$sysmonConfig)
+
+
+	# Backup all configs to single .ps1 file named <hostname>.config.ps1
+		# Backup original audit configuration
 		Write-Output "`$auditBackup `= `@`"" | Out-File "$backupDirectory\$i.config.ps1" -Force
 		$auditBackup | Add-Content "$backupDirectory\$i.config.ps1"
 		Write-Output "`"`@" | Add-Content "$backupDirectory\$i.config.ps1"
 		Write-Output "" | Add-Content "$backupDirectory\$i.config.ps1"
-
+		# Backup Registry values before PS script block logging, transcription, and WEF
 		Write-Output "`$regBackup `= `@`"" | Add-Content "$backupDirectory\$i.config.ps1"
 		$regBackup | Add-Content "$backupDirectory\$i.config.ps1"
 		Write-Output "`"`@" | Add-Content "$backupDirectory\$i.config.ps1"
 		Write-Output "" | Add-Content "$backupDirectory\$i.config.ps1"
-
+		# Backup number of SubscriptionManagers configured before adding ours
 		Write-Output "`$wecNum `= $wecNum" | Add-Content "$backupDirectory\$i.config.ps1"
+		Write-Output "" | Add-Content "$backupDirectory\$i.config.ps1"
+		# Backup sysmon schema configuration
+		Write-Output "`$sysmonBackup `= `@`"" | Add-Content "$backupDirectory\$i.config.ps1"
+		$sysmonBackup | Add-Content "$backupDirectory\$i.config.ps1"
+		Write-Output "`"`@" | Add-Content "$backupDirectory\$i.config.ps1"
 
 	# Cleanup Session
 		Remove-PSSession $s
@@ -184,14 +280,14 @@ function Restore-Sensors
 	{
 		$s = New-PSSession -ComputerName $i
 
-		# Read in original configuration
-		. ".\$i.config.ps1"
+		# Read in backup configuration
+		. "$backupDirectory\$i.config.ps1"
 
 	# Audit Cleanup
 		# Restore original config
 		Invoke-Command -Session $s -Script {
 			param([string[]]$auditBackup)
-			$auditBackup | Out-File "C:\$(hostname).audit.orig.csv"
+			$auditBackup | Out-File "C:\$(hostname).audit.orig.csv" -Force
 			&auditpol.exe /restore /file:C:\$(hostname).audit.orig.csv
 			Remove-Item "C:\$(hostname).audit.orig.csv"
 		} -Args (,$auditBackup)
@@ -228,6 +324,21 @@ function Restore-Sensors
 
 	# Sysmon
 		#Uninstall:  Sysmon.exe –u
+		if ($sysmonBackup -contains "DNE") {
+			Write-Warning "Sysmon not present on deployment. Uninstall."
+			Invoke-Command -Session $s -ScriptBlock {
+				&sysmon -u
+			}
+		}
+		else {
+			Write-Warning "Sysmon present before deployment. Do nothing."
+			#Invoke-Command -Session $s -ScriptBlock {
+			#	param([string[]]$sysmonBackup)
+			#	$sysmonBackup | Out-File "C:\$(hostname).sysmon.orig.xml" -Force
+			#	#&sysmon -c "C:\$(hostname).sysmon.orig.xml"
+			#} -Args (,$sysmonBackup)
+		}
+
 
 	# Cleanup Session
 		Remove-PSSession $s
